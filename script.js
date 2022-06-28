2f3d0275f6e17d70351df966208ad5a384b538eb
Sahil Parsaniya <sahil@webtheuniverse.com>
2022-06-28 12:33:42 +0530

    added Media Queries and completed the given task

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ for(let i=0; i<btnShow.length; i++){
 btnClose.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function(e){
+btnShow[0].addEventListener('keydown', function(e){
     console.log(e);
     if(e.key === 'Escape'){
         if(!modal.classList.contains('hidden')){
